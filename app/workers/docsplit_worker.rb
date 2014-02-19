b22a629642a79abb359cd87e789dadee87558bf3
kee <ke.yang@dokec.com>
2014-02-19 15:28:34 +0800

    delete cache & extract images after completed carrierwave

--- app/workers/docsplit_worker.rb
+++ app/workers/docsplit_worker.rb
@@ -9,15 +9,15 @@ class DocsplitWorker
 			next if item == '.' or item == '..'
 			extract_path = cache_path + "/" + item
 			Docsplit.extract_images(extract_path, :size => %w{1000x}, :format => :png, output: cache_path )
-			#File.delete(extract_path)
+			File.delete(extract_path)
 		end
 		Dir.foreach(cache_path) do |item|
 			next if item == '.' or item == '..'
 			@photo = attach.photos.new
 			@photo.image = File.open(cache_path + "/" + item)
 			@photo.save
-			#FileUtils.rm(cache_path + "/" + item)
+			FileUtils.rm(cache_path + "/" + item)
 		end
-		#Dir.rmdir(cache_path)
+		Dir.rmdir(cache_path)
 	end
 end
