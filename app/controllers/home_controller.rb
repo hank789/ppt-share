f5b4b09474ca86f67acda3369fb0959f4ef4e004
hank <hank.wang@dokec.com>
2014-02-25 13:44:16 +0800

    show four hot slides in one row

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     drop_breadcrumb("首页", root_path)
     set_seo_meta("#{t("menu.slogan")}")
     @slides = Slide.high_likes.last_week_created.fields_for_list.includes(:user).paginate(:page => params[:page], :per_page => 6)
+    @slides_col_md=3;
   end
 
   def api
