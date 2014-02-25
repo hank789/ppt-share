2a1a45fa91471792b42173475f5e18447ba3d9c0
hank <hank.wang@dokec.com>
2014-02-25 14:16:24 +0800

    set hot slides number to 8

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     drop_breadcrumb("首页", root_path)
     set_seo_meta("#{t("menu.slogan")}")
-    @slides = Slide.high_likes.last_week_created.fields_for_list.includes(:user).paginate(:page => params[:page], :per_page => 6)
+    @slides = Slide.high_likes.last_week_created.fields_for_list.includes(:user).paginate(:page => params[:page], :per_page => 8)
     @slides_col_md=3;
   end
 
