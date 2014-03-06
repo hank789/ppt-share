aeb6dbeb8b0b6b63da9b74f324ebc5c51ede0f06
hank <hank.wang@dokec.com>
2014-03-06 17:36:54 +0800

    set default configs

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,7 @@ class Photo
   include Mongoid::BaseModel
 
   field :image
+  field :order_number
 
   belongs_to :user
 	belongs_to :attach
--- app/workers/docsplit_worker.rb
+++ app/workers/docsplit_worker.rb
@@ -4,20 +4,21 @@ class DocsplitWorker
 
 	def perform(attach_id)
 		attach = Attach.find(attach_id)
-		cache_path = Rails.root.join('public', 'uploads', 'tmp', attach.cache_id).to_s
-	  Dir.foreach(cache_path) do |item|
-			next if item == '.' or item == '..'
-			extract_path = cache_path + "/" + item
-			Docsplit.extract_images(extract_path, :size => %w{1000x}, :format => :png, output: cache_path )
-			File.delete(extract_path)
-		end
-		Dir.foreach(cache_path) do |item|
-			next if item == '.' or item == '..'
-			@photo = attach.photos.new
-			@photo.image = File.open(cache_path + "/" + item)
-			@photo.save
-			FileUtils.rm(cache_path + "/" + item)
-		end
-		Dir.rmdir(cache_path)
+    cache_path = Rails.root.join('public', 'uploads', 'tmp', attach.cache_id).to_s
+    Dir.foreach(cache_path) do |item|
+      next if item == '.' or item == '..'
+      extract_path = cache_path + "/" + item
+      Docsplit.extract_images(extract_path, :size => %w{1000x}, :format => :png, output: cache_path )
+      File.delete(extract_path)
+    end
+    Dir.foreach(cache_path) do |item|
+      next if item == '.' or item == '..'
+      @photo = attach.photos.new
+      @photo.order_number = item
+      @photo.image = File.open(cache_path + "/" + item)
+      @photo.save
+      FileUtils.rm(cache_path + "/" + item)
+    end
+    Dir.rmdir(cache_path)
 	end
 end
