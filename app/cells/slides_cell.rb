210d5b50f3f927e919004a231b0af6c1a607f8f2
hank <hank.wang@dokec.com>
2014-03-08 15:14:15 +0800

    set slide list show only one image

--- app/cells/slides_cell.rb
+++ app/cells/slides_cell.rb
@@ -14,7 +14,7 @@ class SlidesCell < BaseCell
 
   def carousel_simple(opts = {})
     @attach = Attach.find(opts[:attach_id]) unless opts[:attach_id].blank?
-    @photos = @attach.photos.asc(:order_number) unless opts[:attach_id].blank?
+    @photos = @attach.photos.asc(:order_number).limit(1) unless opts[:attach_id].blank?
     render
   end
 
