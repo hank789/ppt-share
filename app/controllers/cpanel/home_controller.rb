355a6b937a575d1f33542bb63b1e30256bf21e1f
PAFA <fan.pang@gmail.com>
2014-03-11 11:47:58 +0800

    Expanding the number of cpanel homepage slides limit

--- app/controllers/cpanel/home_controller.rb
+++ app/controllers/cpanel/home_controller.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 class Cpanel::HomeController < Cpanel::ApplicationController
   def index
-    @recent_slides = Slide.recent.limit(5)
+    @recent_slides = Slide.recent.limit(25)
   end
 end
