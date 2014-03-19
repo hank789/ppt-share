063698f87fc5bd35e50878b36bb21273d8a14ed6
hank <hank.wang@dokec.com>
2014-03-19 17:01:21 +0800

    增加用户头像自定义class的属性

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -41,20 +41,20 @@ module UsersHelper
 
   def user_avatar_tag(user, size = :normal, opts = {})
     link = opts[:link] || false
-
+    custom_class = opts[:class] || ''
     width = user_avatar_width_for_size(size)
 
     if user.blank?
       # hash = Digest::MD5.hexdigest("") => d41d8cd98f00b204e9800998ecf8427e
-      return image_tag("avatar/#{size}.png", :class => "uface")
+      return image_tag("avatar/#{size}.png", :class => "uface #{custom_class}")
     end
 
     if user[:avatar].blank?
       default_url = asset_path("avatar/#{size}.png")
       img_src = "#{Setting.gravatar_proxy}/avatar/#{user.email_md5}.png"
-      img = image_tag(img_src, :class => "uface", :style => "width:#{width}px;height:#{width}px;")
+      img = image_tag(img_src, :class => "uface #{custom_class}", :style => "width:#{width}px;height:#{width}px;")
     else
-      img = image_tag(user.avatar.url(user_avatar_size_name_for_2x(size)), :class => "uface", :style => "width:#{width}px;height:#{width}px;")
+      img = image_tag(user.avatar.url(user_avatar_size_name_for_2x(size)), :class => "uface #{custom_class}", :style => "width:#{width}px;height:#{width}px;")
     end
 
     if link
