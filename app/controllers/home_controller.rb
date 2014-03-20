d433f4f27e43d485b0d5c515fa7176919065261a
hank <hank.wang@dokec.com>
2014-03-20 14:30:36 +0800

    完成首页今日推荐加精的ppt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
       @activities_followed = PublicActivity::Activity.desc(:created_at).where(:key.ne => "reply.mention").where(:owner_id.in => current_user.follower_ids).paginate(:page => params[:page], :per_page => 20)
       @activities_with_me = PublicActivity::Activity.desc(:created_at).where(:recipient_id => current_user.id).paginate(:page => params[:page], :per_page => 20)
       @activities_important = PublicActivity::Activity.desc(:created_at).where(:owner_id.in => current_user.follower_ids).where(:trackable_type => "Slide").paginate(:page => params[:page], :per_page => 20)
+      @slides_suggest = Slide.excellent.recent.fields_for_list.where(:user_id.ne => current_user.id).limit(6)
       drop_breadcrumb("首页", root_path)
       set_seo_meta("#{t("menu.slogan")}")
     else
